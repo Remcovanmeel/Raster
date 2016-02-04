d674f8f341657dd9fa1a8bb2510c91fa259027cf
Carlo Palinckx <cpalinckx@rb-media.nl>
2016-02-04 20:08:11 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>              
+  <meta charset="utf-8" />
+  <meta name="robots" content="noindex, nofollow" />
+  <meta name="author" content="BIC Improvement, info@bicimprove.com" />
+</head>
+
+<body>
+  <script type="text/javascript" src="js/raster.js"></script> 
+</body>
