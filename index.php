eca38d0f933453a071cc18737366e97d3e3ce976
Carlo Palinckx <cpalinckx@rb-media.nl>
2016-02-19 13:51:43 +0100

    Built basic dashboard functionality

--- index.php
+++ index.php
@@ -3,14 +3,18 @@
 <head>              
   <meta charset="utf-8" />
   <meta name="robots" content="noindex, nofollow" />
-  <meta name="author" content="BIC Improvement, info@bicimprove.com" />
+  <meta name="author" content="" />
 
   <style>
     body { height: 5000px; }
+    #visuals { height: 100%; }
   </style>
 </head>
 
 <body>
+  <div id="visuals">
+    
+  </div>
     <script type="text/javascript" src="raster/modules/visual.js"></script>
     <script type="text/javascript" src="raster/modules/caching.js"></script>
     <script type="text/javascript" src="raster/modules/events.js"></script>
